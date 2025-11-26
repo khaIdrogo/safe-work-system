b66b2f3d3f63171dd71a2e3759f4e4b3900106fc
khaIdrogo <100453887+khaIdrogo@users.noreply.github.com>
2025-11-26 14:02:23 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.https://vwqcehjjtufbrqzvscpt.supabase.co!;
+const supabaseKey = process.env.sb_publishable__6bTQj-CVRBxFCPQj3aMEg_NuawcJfN!;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase env vars. Set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY.');
 }
