7138523e39c46fae36c5c1d42633e0fd2f1cbafc
khaIdrogo <100453887+khaIdrogo@users.noreply.github.com>
2025-11-26 14:13:16 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { output: "standalone" };
 module.exports = nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
